c37962026782f5ce0974eea63bb11e13e88e0e9a
Vincent Jeannin <jeannin.vincent@gmail.com>
2024-01-11 23:36:05 +0100

    feat(#2 #3) : Add Font map

---
+++ WarStreamer.Interfaces/Maps/IFontMap.cs
@@ -0,0 +1,15 @@
+﻿using WarStreamer.ViewModels;
+
+namespace WarStreamer.Interfaces.Maps
+{
+    public interface IFontMap
+    {
+        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+        |*                           PUBLIC METHODS                          *|
+        \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+        public List<FontViewModel> GetAll();
+
+        public FontViewModel? GetById(int id);
+    }
+}
---
+++ WarStreamer.Maps/FontMap.cs
@@ -0,0 +1,49 @@
+﻿using WarStreamer.Interfaces.Maps;
+using WarStreamer.Interfaces.Services;
+using WarStreamer.Models;
+using WarStreamer.ViewModels;
+
+namespace WarStreamer.Maps
+{
+    public class FontMap(IFontService service) : IFontMap
+    {
+        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+        |*                               FIELDS                              *|
+        \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+        private readonly IFontService _service = service;
+
+        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+        |*                           PUBLIC METHODS                          *|
+        \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+        public List<FontViewModel> GetAll()
+        {
+            return DomainToViewModel(_service.GetAll());
+        }
+
+        public FontViewModel? GetById(int id)
+        {
+            Font? font = _service.GetById(id);
+            return font != null ? DomainToViewModel(font) : null;
+        }
+
+        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+        |*                          PRIVATE METHODS                          *|
+        \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+        /* * * * * * * * * * * * * * * * * *\
+        |*              STATIC             *|
+        \* * * * * * * * * * * * * * * * * */
+
+        private static FontViewModel DomainToViewModel(Font domain)
+        {
+            return new(domain.Id, domain.DisplayName, domain.FileName);
+        }
+
+        private static List<FontViewModel> DomainToViewModel(List<Font> domain)
+        {
+            return domain.Select(DomainToViewModel).ToList();
+        }
+    }
+}
